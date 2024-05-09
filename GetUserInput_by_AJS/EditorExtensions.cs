5b5e79a99568389742c104b3f36c677807d2cc91
AutoLISP-Just-Simple <admin@lisp.vn>
2024-05-09 17:45:27 +0700

    Add project files.

---
+++ GetUserInput_by_AJS/EditorExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using System.Diagnostics;
+
+namespace GetUserInput_by_AJS
+{
+    public static class EditorExtensions
+    {
+        public static PromptStatus GetResult = PromptStatus.None;
+
+        public static Point3d GetPointWCS(this Editor ed, string msg = "\nSpecify a point", bool allowNone = true)
+        {
+            var ppo = new PromptPointOptions(msg);
+            ppo.AllowNone = allowNone;
+
+            var psr = ed.GetPoint(ppo);
+            GetResult = psr.Status;
+
+            //Example by www.lisp.vn
+            return psr.Status == PromptStatus.OK ? psr.Value.ToWCS() : Point3d.Origin;
+        }
+
+        public static ObjectId ToDatabase(this Entity e)
+        {
+            ObjectId retId = ObjectId.Null;
+            var db = HostApplicationServices.WorkingDatabase;
+            using (var tr = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction())
+            {
+                var btr = tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+                retId = btr.AppendEntity(e);
+                tr.AddNewlyCreatedDBObject(e, true);
+                tr.Commit();
+            }
+            //Example by www.lisp.vn
+            return retId;
+        }
+
+        public static Line GetLine(this Editor ed)
+        {
+            var ppo = new PromptPointOptions("\nSpecify first point");
+            ppo.AllowNone = true;
+            var ppr = ed.GetPoint(ppo);
+            if (ppr.Status != PromptStatus.OK) return null;
+
+            var p_UCS = ppr.Value;
+
+            var p1 = p_UCS.ToWCS();
+
+            ppo.Message = "\nSpecify second point";
+            ppo.BasePoint = p1.ToUCS();
+            ppo.UseBasePoint = true;
+
+            ppr = ed.GetPoint(ppo);
+            if (ppr.Status != PromptStatus.OK) return null;
+
+            var p2 = ppr.Value.ToWCS();
+
+            //Example by www.lisp.vn
+            return new Line(p1, p2);
+        }
+
+        public static Point3d ToWCS(this Point3d pt)
+        {
+            //Example by www.lisp.vn
+            return pt.TransformBy(Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor.CurrentUserCoordinateSystem);
+        }
+
+        public static Point3d ToUCS(this Point3d pt)
+        {
+            //Example by www.lisp.vn
+            return pt.TransformBy(Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor.CurrentUserCoordinateSystem.Inverse());
+        }
+
+        public static void Princ(string msg)
+            => Debug.Print(msg);
+    }
+}
---
+++ GetUserInput_by_AJS/myCommands.cs
@@ -0,0 +1,52 @@
+﻿// (C) Copyright 2024 by
+//
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+// This line is not mandatory, but improves loading performances
+[assembly: CommandClass(typeof(GetUserInput_by_AJS.MyCommands))]
+
+namespace GetUserInput_by_AJS
+{
+    public partial class MyCommands
+    {
+        [CommandMethod("GetPointWCS", CommandFlags.Modal)]
+        public void GetPointWCS()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            if (doc != null)
+            {
+                Editor ed = doc.Editor;
+
+                var pt = ed.GetPointWCS();
+                if (EditorExtensions.GetResult == PromptStatus.OK)
+                {
+                    Circle cc = new Circle(pt, Autodesk.AutoCAD.Geometry.Vector3d.ZAxis, 10);
+                    cc.ToDatabase();
+                }
+            }
+            //Example by www.lisp.vn
+        }
+
+        [CommandMethod("Get2Point", CommandFlags.Modal)]
+        public void Get2Point_WCS()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed;
+            if (doc != null)
+            {
+                ed = doc.Editor;
+                Line ln = ed.GetLine();
+                if (ln != null)
+                {
+                    ln.ToDatabase();
+                }
+                return;
+            }
+            //Example by www.lisp.vn
+        }
+    }
+}
